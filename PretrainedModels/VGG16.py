2e20bbedaf6180cb21ee59073c8896f0b0993724
loplace <ronci.federico.1993@gmail.com>
2018-08-21 16:04:34 +0200

    VGG16 transfer learning

--- PretrainedModels/VGG16.py
+++ PretrainedModels/VGG16.py
@@ -14,8 +14,8 @@ vgg_conv = vgg16.VGG16(weights='imagenet',
 train_dir = '/home/federico/PycharmProjects/Image Classification/Datasets/Male_Female/train'
 validation_dir = '/home/federico/PycharmProjects/Image Classification/Datasets/Male_Female/validation'
 
-nTrain = 1945
-nVal = 833
+nTrain = 184
+nVal = 165
 
 datagen = ImageDataGenerator(
       rescale=1./255,
@@ -27,23 +27,14 @@ datagen = ImageDataGenerator(
 batch_size = 20
 
 train_features = np.zeros(shape=(nTrain, 7, 7, 512))
-train_labels = np.zeros(shape=(nTrain, 3))
-
-validation_features = np.zeros(shape=(nVal, 7, 7, 512))
-validation_labels = np.zeros(shape=(nVal, 2))
+train_labels = np.zeros(shape=(nTrain))
 
 train_generator = datagen.flow_from_directory(
     train_dir,
     target_size=(224, 224),
     batch_size=batch_size,
     class_mode='binary',
-    shuffle=False)
-
-validation_generator = datagen.flow_from_directory(
-        validation_dir,
-        target_size=(224, 224),
-        batch_size=batch_size,
-        class_mode='binary')
+    shuffle=True)
 
 i = 0
 for inputs_batch, labels_batch in train_generator:
@@ -51,34 +42,39 @@ for inputs_batch, labels_batch in train_generator:
     train_features[i * batch_size: (i + 1) * batch_size] = features_batch
     train_labels[i * batch_size: (i + 1) * batch_size] = labels_batch
     i += 1
+    print('a')
     if i * batch_size >= nTrain:
         break
 
 train_features = np.reshape(train_features, (nTrain, 7 * 7 * 512))
 
-for val_batch, labels_val in validation_generator:
-    features_batch = vgg_conv.predict(val_batch)
-    validation_features[i * batch_size: (i + 1) * batch_size] = val_batch
-    validation_labels[i * batch_size: (i + 1) * batch_size] = labels_val
+validation_features = np.zeros(shape=(nVal, 7, 7, 512))
+validation_labels = np.zeros(shape=(nVal))
+
+validation_generator = datagen.flow_from_directory(
+    validation_dir,
+    target_size=(224, 224),
+    batch_size=batch_size,
+    class_mode='binary',
+    shuffle=False)
+
+i = 0
+for inputs_batch, labels_batch in validation_generator:
+    features_batch = vgg_conv.predict(inputs_batch)
+    validation_features[i * batch_size : (i + 1) * batch_size] = features_batch
+    validation_labels[i * batch_size : (i + 1) * batch_size] = labels_batch
     i += 1
+    print ('b')
     if i * batch_size >= nVal:
         break
 
-validation_features = np.reshape(train_features, (nTrain, 7 * 7 * 512))
-
+validation_features = np.reshape(validation_features, (nVal, 7 * 7 * 512))
 
 #Generate Model
-# Create the model
 model = models.Sequential()
-
-# Add the vgg convolutional base model
-model.add(vgg_conv)
-
-# Add new layers
-model.add(layers.Flatten())
-model.add(layers.Dense(1024, activation='relu'))
+model.add(layers.Dense(512, activation='relu', input_dim=7 * 7 * 512))
 model.add(layers.Dropout(0.5))
-model.add(layers.Dense(3, activation='softmax'))
+model.add(layers.Dense(1, activation='softmax'))
 
 model.compile(optimizer=optimizers.RMSprop(lr=2e-4),
               loss='binary_crossentropy',
@@ -88,4 +84,6 @@ history = model.fit(train_features,
                     train_labels,
                     epochs=20,
                     batch_size=batch_size,
-                    validation_data=(validation_features, validation_labels))
+                    validation_data=(validation_features,validation_labels))
+
+print ('done')
