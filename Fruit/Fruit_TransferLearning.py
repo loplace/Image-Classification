873b118792178bbd20203689f0a9933c8d4b666d
Federico <ronci.federico.1993@gmail.com>
2018-09-06 17:13:32 +0200

    Final Commit

--- Fruit/Fruit_TransferLearning.py
+++ Fruit/Fruit_TransferLearning.py
@@ -1,24 +1,28 @@
 import numpy as np
-from keras import models, layers, optimizers
+from keras import models, layers
 from keras.applications import vgg16
 # load vgg model with weights by imagenet
 from keras.utils import to_categorical
 from keras_preprocessing.image import ImageDataGenerator
-from sklearn import metrics
+
+from Utilities.Metrics import Metrics
 
 vgg_conv = vgg16.VGG16(weights='imagenet',
                        include_top=False,
                        input_shape=(100, 100, 3))
 
-train_dir = '/home/federico/PycharmProjects/Image Classification/Datasets/fruits/fruits-360/Training'
-validation_dir = '/home/federico/PycharmProjects/Image Classification/Datasets/fruits/fruits-360/Test'
+vgg_conv.summary()
 
-nTrain = 37836
-nVal = 12709
-batch_size = 10
+
+train_dir = 'C:/Users/Federico/PycharmProjects/Image-Classification/Datasets/fruit/Training'
+validation_dir = 'C:/Users/Federico/PycharmProjects/Image-Classification/Datasets/fruit/Test'
+
+nTrain = 38695
+nVal = 13000
+batch_size = 100
 image_size = 100
 
-epochs = 1
+epochs = 10
 
 datagen = ImageDataGenerator(
     rescale=1. / 255,
@@ -28,14 +32,15 @@ datagen = ImageDataGenerator(
     horizontal_flip=True,
     fill_mode='nearest')
 
-train_features = np.zeros(shape=(nTrain, 7, 7, 512))
-train_labels = np.zeros(shape=nTrain)
+train_features = np.zeros(shape=(nTrain, 3, 3, 512))
+train_labels = np.zeros(shape=(nTrain, 77))
 
 train_generator = datagen.flow_from_directory(
     train_dir,
     target_size=(image_size, image_size),
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='categorical',
+    shuffle=True)
 
 i = 0
 for inputs_batch, labels_batch in train_generator:
@@ -46,18 +51,18 @@ for inputs_batch, labels_batch in train_generator:
     if i * batch_size >= nTrain:
         break
 
-train_features = np.reshape(train_features, (nTrain, 7 * 7 * 512))
+train_features = np.reshape(train_features, (nTrain, 3 * 3 * 512))
 
 validation_datagen = ImageDataGenerator(rescale=1. / 255)
 
-validation_features = np.zeros(shape=(nVal, 7, 7, 512))
-validation_labels = np.zeros(shape=nVal)
+validation_features = np.zeros(shape=(nVal, 3, 3, 512))
+validation_labels = np.zeros(shape=(nVal,77))
 
 validation_generator = validation_datagen.flow_from_directory(
     validation_dir,
     target_size=(image_size, image_size),
     batch_size=batch_size,
-    class_mode='binary',
+    class_mode='categorical',
     shuffle=False)
 
 i = 0
@@ -69,26 +74,27 @@ for inputs_batch, labels_batch in validation_generator:
     if i * batch_size >= nVal:
         break
 
-validation_features = np.reshape(validation_features, (nVal, 7 * 7 * 512))
+validation_features = np.reshape(validation_features, (nVal, 3 * 3 * 512))
 
 # Generate Model
 model = models.Sequential()
-model.add(layers.Dense(512, activation='relu', input_dim=7 * 7 * 512))
+model.add(layers.Dense(512, activation='relu', input_dim=3 * 3 * 512))
 model.add(layers.Dropout(0.25))
-model.add(layers.Dense(75, activation='sigmoid'))
+model.add(layers.Dense(77, activation='softmax'))
 
-model.compile(optimizer=optimizers.RMSprop(lr=2e-4),
-              loss='binary_crossentropy',
+model.compile(optimizer='Adadelta',
+              loss='categorical_crossentropy',
               metrics=['acc'])
 
+metrics_epoch = Metrics()
 history = model.fit(train_features,
                     train_labels,
                     epochs=epochs,
                     batch_size=batch_size,
-                    validation_data=(validation_features, validation_labels))
+                    validation_data=(validation_features, validation_labels),
+                    callbacks=[metrics_epoch])
 
 model.save('Fruit_TransferLearning.h5')
-
 
 fnames = validation_generator.filenames
 ground_truth = validation_generator.classes
@@ -104,47 +110,25 @@ print(val_preds)
 val_trues = validation_generator.classes
 classes_one_hot_encoded = to_categorical(val_trues)
 
-cm = metrics.confusion_matrix(val_trues, val_preds)
-print(cm)
 
-precisions, recall, fscore, support = metrics.precision_recall_fscore_support(val_trues, val_preds, average=None)
+f = open("Fruit_TransferLearning.txt", "w+")
 
-# Plot the accuracy and loss curves
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-# metrics calculated by using sklearn after validating
-print('Precision')
-print(precisions)
-print('Recall')
-print(recall)
-print('Fscore')
-print(fscore)
-
-f = open("Fruit_FineTuning.txt", "w+")
-
-f.write('Number of Epochs:' + epochs + '\n')
+f.write('Number of Epochs:' + str(epochs) + '\n')
 
 f.write('Weighted Precision:\n')
-str1 = str(precisions)
+str1 = str(metrics_epoch.val_precisions)
 f.write(str1 + '\n')
 
 f.write('Weighted Recall:\n')
-str2 = str(recall)
+str2 = str(metrics_epoch.val_recalls)
 f.write(str2 + '\n')
 
 f.write('F_Score:\n')
-str3 = str(fscore)
+str3 = str(metrics_epoch.val_f1s)
 f.write(str3 + '\n')
 
 f.write('val_Acc:\n')
-str3 = str(val_acc)
-f.write(str3 + '\n')
-
-f.write('val_loss:\n')
-str3 = str(val_loss)
+str3 = str(metrics_epoch.val_accuracy)
 f.write(str3 + '\n')
 
 f.close()
